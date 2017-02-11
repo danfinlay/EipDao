4658451576311edbe66ad64481fee5d7c5aa216e
Dan Finlay <dan@danfinlay.com>
2017-02-10 17:27:09 -0800

    Remove frontend, expect to use ENS

--- src/tests.sol
+++ src/tests.sol
@@ -1,28 +1,14 @@
 pragma solidity ^0.4.0;
 
 import "dapple/test.sol";
-import "./EipDaoFrontend.sol";
 import "./EipVotesController.sol";
 
 contract UnitTest is Test {
 
-  EipVotesFrontend dao;
+  EipVotesController dao;
 
   function setUp () {
-    dao = new EipVotesFrontend();
-  }
-
-  function testSetController() {
-    address first = 0x01;
-    address second = 0x02;
-
-    dao.setController(first);
-    address firstResult = dao.getController();
-    assertEq(firstResult, first);
-
-    dao.setController(second);
-    address secondResult = dao.getController();
-    assertEq(secondResult, second);
+    dao = new EipVotesController();
   }
 
 }
